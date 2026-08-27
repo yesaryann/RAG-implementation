c2cdb48d5b4ff27c7b2f831d0945a9802f97e5ba
Raj Aryan <i.rajaryann@gmail.com>
2026-08-27 23:30:09 +0530

    2.Now convert question in vector embedding

--- query.js
+++ query.js
@@ -2,7 +2,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import readlineSync from 'readline-sync';
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
 
+const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GEMINI_API_KEY,
+    model: 'text-embedding-004',
+    });
+ 
+ const queryVector = await embeddings.embedQuery(question);   
+ 
 async function main(){
    const userProblem = readlineSync.question("Ask me anything--> ");
    await chatting(userProblem);
