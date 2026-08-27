97636252203014af7a923620e3a26ac5d97eb75e
Raj Aryan <i.rajaryann@gmail.com>
2026-08-27 23:39:54 +0530

    3.search the relevant documents into vector DB

--- query.js
+++ query.js
@@ -3,14 +3,29 @@ dotenv.config();
 
 import readlineSync from 'readline-sync';
 import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { Pinecone } from '@pinecone-database/pinecone';
 
 const embeddings = new GoogleGenerativeAIEmbeddings({
     apiKey: process.env.GEMINI_API_KEY,
     model: 'text-embedding-004',
     });
  
- const queryVector = await embeddings.embedQuery(question);   
- 
+const queryVector = await embeddings.embedQuery(question); 
+
+const pinecone = new Pinecone();
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+const searchResults = await pineconeIndex.query({
+    topK: 10,
+    vector: queryVector,
+    includeMetadata: true,
+    });
+    
+    
+const context = searchResults.matches
+                   .map(match => match.metadata.text)
+                   .join("\n\n---\n\n");
+
 async function main(){
    const userProblem = readlineSync.question("Ask me anything--> ");
    await chatting(userProblem);
